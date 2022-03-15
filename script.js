a88f10945ad36c87ef4a5814773f51d21a3a92cb
JoshWhipple17 <joshwhipple13@gmail.com>
2022-03-14 21:09:42 -0400

    Declare all variables and functions

---
+++ script.js
@@ -0,0 +1,32 @@
+// Theme: Things someone forgot
+// Output: (Pronoun) forgot to (Action) (Number) (Noun).
+/* TODO:
+ - Build arrays for Pronoun, Action, Number, Noun
+ - Create a random selector for Pronoun, Action, Number, Noun
+ - Combine all the randomly selected elements from Pronoun, Action, Number, Noun
+*/
+
+const pronouns = [];
+const actions = [];
+const numbers = [];
+const nouns = [];
+
+const randomPronoun = () => {
+
+};
+
+const randomAction = () => {
+
+};
+
+const randomNumber = () => {
+
+};
+
+const randomNoun = ()  => {
+
+};
+
+const createRandomMessage = () => {
+    return "(Pronoun) forgot to (Action) (Number) (Noun).";
+};
